83f8e495df0cf40226b51e6aba1848bbbf44d1a0
Gitahi Ng'ang'a <gitahi86@gmail.com>
2014-03-21 19:17:18 +0300

    Created basic project structure added .gitignore and updated README.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# This is my README
+ADT Migrator
+=====================================
+
+Overview
+--------
+A tool to migrate ADT data from the legacy MS Access database to the new FDT MySQL platform.
---
+++ src/main/java/com/intellisoftkenya/adt/migrator/App.java
@@ -0,0 +1,13 @@
+package com.intellisoftkenya.adt.migrator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
