fde0e5046def6db38b540d0eb7adcc0701f867ff
Gitahi Ng'ang'a <gitahi86@gmail.com>
2014-05-18 01:46:50 +0300

    Now database connection and logging leve properties are managed inside a properties file editable through a GUI.

---
+++ src/main/java/com/intellisoftkenya/onetooner/PropertyManager.java
@@ -0,0 +1,110 @@
+package com.intellisoftkenya.onetooner;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author gitahi
+ */
+public class PropertyManager {
+
+    private static final String propertiesFile = "settings.properties";
+    private static final Properties properties = new Properties();
+
+    static {
+        loadProperties();
+    }
+
+    /**
+     * @param name the property key
+     * @param def if true, the default value rather that the current property
+     * value is returned
+     *
+     * @return the property value
+     */
+    public static String getProperty(String name, boolean def) {
+        if (!def) {
+            return properties.getProperty(name);
+        }
+        if (name != null) {
+            switch (name) {
+                case "source.driver":
+                    return "sun.jdbc.odbc.JdbcOdbcDriver";
+                case "source.url":
+                    return "jdbc:odbc:Driver={Microsoft Access Driver (*.mdb, *.accdb)}; "
+                            + "DBQ=C:\\ARVDatabase\\Datafiles\\ARVDispensingDatabase_be.mdb";
+                case "source.username":
+                    return "";
+                case "source.password":
+                    return "";
+                case "destination.driver":
+                    return "com.mysql.jdbc.Driver";
+                case "destination.url":
+                    return "jdbc:mysql://localhost:3306/fdt";
+                case "destination.username":
+                    return "root";
+                case "destination.password":
+                    return "2806";
+                case "logging.level":
+                    return "INFO";
+            }
+        }
+        return "";
+    }
+
+    public static String getProperty(String name) {
+        return getProperty(name, false);
+    }
+
+    public static void setProperty(String key, String value) {
+        properties.setProperty(key, value);
+    }
+
+    public static void saveProperties() {
+        OutputStream outputStream = null;
+        try {
+            outputStream = new FileOutputStream(propertiesFile);
+            properties.store(outputStream, null);
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(PropertyManager.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(PropertyManager.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            if (outputStream != null) {
+                try {
+                    outputStream.close();
+                } catch (IOException ex) {
+                    Logger.getLogger(PropertyManager.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+    }
+
+    private static void loadProperties() {
+        InputStream inputStream = null;
+        try {
+            inputStream = new FileInputStream(propertiesFile);
+            properties.load(inputStream);
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(PropertyManager.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(PropertyManager.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            if (inputStream != null) {
+                try {
+                    inputStream.close();
+                } catch (IOException ex) {
+                    Logger.getLogger(PropertyManager.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+    }
+}
